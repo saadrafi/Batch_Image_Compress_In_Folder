47bb059a1cb2551d34a79aedfb85119266020bfa
saadrafi <saadrafi81@gmail.com>
2025-02-13 19:05:01 +0600

    added requirements file

--- compress_images_of_folder.py
+++ compress_images_of_folder.py
@@ -1,13 +1,17 @@
 import os
 from PIL import Image
 
+output_folder = "compressed_images"
+type = "webp"
+
 # Function to compress a single image
-def compress_image(input_path, output_path, type="webp", quality=85):
+def compress_image(input_path, output_path, type=type, quality=85):
     with Image.open(input_path) as img:
         img.save(f"{output_path}.{type}", type, quality=quality)
 
 # Function to compress all images in a given folder
-def compress_images_in_folder(folder_name, output_folder="compressed_images", type="webp", quality=85):
+def compress_images_in_folder(folder_name, output_folder=output_folder, type=type, quality=85):
+
     # Create the output folder if it doesn't exist
     os.makedirs(output_folder, exist_ok=True)
     
